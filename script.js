8fe75a18cb285b2f7ea1dbe5170beff9495be779
silvawde <53557641+silvawde@users.noreply.github.com>
2021-02-10 16:38:29 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("h1")
+
+// and when i scroll calculate how big the font size should be
+window.addEventListener("scroll", function(){
+  const pixels = window.pageYOffset
+  
+  const fontWeight = pixels * 0.4 + 100
+  const fontWidth = pixels * 0.1 + 100
+
+  h1.style.fontVariationSettings = `"wght" ${fontWeight}, "wdth" ${fontWidth}`
+})
